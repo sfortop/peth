a0025d26a65a0a341c7e8e2ca557f4cc2b67c0b1
serhii-borodai <serhii.borodai@globalgames.net>
2018-07-16 19:09:00 +0300

    change eth node env var

--- config/autoload/ethereum.global.php
+++ config/autoload/ethereum.global.php
@@ -9,7 +9,7 @@
 
 return [
     'ethereum' => [
-        'host' => getenv('GETH_NODE_IP'),
+        'host' => parse_url(getenv('ETHEREUM_NODE_URL'))['host'] ?? null,
         'contracts' => [
             'DMT' => '0x2ccbff3a042c68716ed2a2cb0c544a9f1d1935e1',
         ],
